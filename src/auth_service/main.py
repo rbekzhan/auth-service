8ae62bcd62939abc6083567bbd873c0028433b8d
Бекжан Рахметжан <bekzhan@Bekzans-MacBook-Pro.local>
2023-09-04 17:34:09 +0600

    port

--- src/auth_service/main.py
+++ src/auth_service/main.py
@@ -2,7 +2,7 @@ from aiomisc import entrypoint
 from auth_service.route import routes
 from auth_service.services.gate_way_service import GateWayService
 
-services = (GateWayService(address="0.0.0.0", port=60009, routes=routes),)
+services = (GateWayService(address="0.0.0.0", port=60004, routes=routes),)
 
 
 def start():
