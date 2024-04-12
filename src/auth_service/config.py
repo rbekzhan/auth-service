081060e245aad722683568423aecb616a350b07d
Bekzhan <44322479+rbekzhan@users.noreply.github.com>
2024-04-12 12:15:51 +0500

    Update config.py

--- src/auth_service/config.py
+++ src/auth_service/config.py
@@ -9,7 +9,7 @@ DB_SERVICE_IP = os.environ.get("DB_SERVICE_IP")
 DB_SERVICE_PORT = os.environ.get("DB_SERVICE_PORT")
 DB_SERVICE_URL = f"{HTTP_PROTOCOL}://{DB_SERVICE_IP}:{DB_SERVICE_PORT}"
 
-SECRET_KEY = 'j3h6xuhcmays446h6f6zfw8sqx9ucfumyzxfppz5rdczkbzux5t9ezzezf7yauzg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 ACCESS_TOKEN_EXPIRATION = 36000
 REFRESH_TOKEN_EXPIRATION = 604800
 redis_client = redis.Redis(host='redis', port=6379, db=0)
